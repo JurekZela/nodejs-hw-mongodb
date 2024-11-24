9bbd73a428b7a54f5694b079d87f7e1b3dccae8c
Juri Zelabowski <juri.zelabowski@gmail.com>
2024-11-24 17:19:30 +0100

    cloud name

--- src/constants/index.js
+++ src/constants/index.js
@@ -13,10 +13,3 @@ export const SMTP = {
 export const TEMPLATES_DIR = path.resolve("src", "templates");
 export const TEMP_UPLOAD_DIR = path.resolve("temp");
 export const UPLOAD_DIR = path.resolve("uploads");
-
-export const CLOUDINARY = {
-  CLOUD_NAME: 'CLOUD_NAME',
-  API_KEY: 'API_KEY',
-  API_SECRET: 'API_SECRET',
-  ENABLE_CLOUDINARY: 'ENABLE_CLOUDINARY',
-};
